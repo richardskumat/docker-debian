bbac1f0e6730da07e4e5da15afbef64655e997f0
Richard Skumat <gitlab@richardskumat.com>
2020-02-23 11:57:38 +0000

    remove gitlab docker registry entries from ci files
    move from python2 to python3 minimal in the molecule images

--- docker-build.sh
+++ docker-build.sh
@@ -8,70 +8,46 @@
 docker build -f Dockerfile-d9 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim \
--t $CI_REGISTRY_IMAGE:9 \
--t $CI_REGISTRY_IMAGE:stretch \
--t $CI_REGISTRY_IMAGE:stretch-slim .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim .
 
 # d9 with contrib and nonfree
 docker build -f Dockerfile-d9nf \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree \
--t $CI_REGISTRY_IMAGE:9-nonfree \
--t $CI_REGISTRY_IMAGE:stretch-nonfree \
--t $CI_REGISTRY_IMAGE:stretch-slim-nonfree .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree .
 
 # d9 with molecule dependencies
 docker build -f Dockerfile-d9-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-molecule \
--t $CI_REGISTRY_IMAGE:9-molecule \
--t $CI_REGISTRY_IMAGE:stretch-molecule \
--t $CI_REGISTRY_IMAGE:stretch-slim-molecule .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-molecule .
 
 # d9 with molecule dependencies and contrib & nonfree repos
 docker build -f Dockerfile-d9nf-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:9-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:stretch-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:stretch-slim-nonfree-molecule .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
 # plain d10
 docker build -f Dockerfile-d10 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim \
--t $CI_REGISTRY_IMAGE:10 \
--t $CI_REGISTRY_IMAGE:buster \
--t $CI_REGISTRY_IMAGE:buster-slim .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim .
 
 # d10 with contrib and nonfree
 docker build -f Dockerfile-d10nf \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree \
--t $CI_REGISTRY_IMAGE:10-nonfree \
--t $CI_REGISTRY_IMAGE:buster-nonfree \
--t $CI_REGISTRY_IMAGE:buster-slim-nonfree .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree .
 
 # d10 with molecule dependencies
 docker build -f Dockerfile-d10-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-molecule \
--t $CI_REGISTRY_IMAGE:10-molecule \
--t $CI_REGISTRY_IMAGE:buster-molecule \
--t $CI_REGISTRY_IMAGE:buster-slim-molecule .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-molecule .
 
 # d10 with molecule dependencies and contrib & nonfree repos
 docker build -f Dockerfile-d10nf-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:10-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:buster-nonfree-molecule \
--t $CI_REGISTRY_IMAGE:buster-slim-nonfree-molecule .
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
