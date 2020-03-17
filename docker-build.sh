8c4cdfe2c98668b8cfcaf7986a80c66e8767e914
Richard Skumat <gitlab@richardskumat.com>
2020-03-17 12:58:01 +0000

    add new dockerfile to build an image with git

--- docker-build.sh
+++ docker-build.sh
@@ -51,3 +51,7 @@ docker build -f Dockerfile-d10nf-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
+
+# git tag for git cloning in ci images
+docker build -f Dockerfile-git \
+-t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
