1ac62e5023404d1cb6a34b67db2ba26c21addbe9
Richard Skumat <gitlab@richardskumat.com>
2020-08-19 19:35:49 +0100

    remove unnecessary images and update ci to try to use buildx for multiarch builds

--- docker-build.sh
+++ docker-build.sh
@@ -1,57 +1,23 @@
 #!/bin/sh
-# use this script to build the docker images with tags
-# this is really dumb, must be a better way to build
-# many tags/images from a script without manual
-# definition
-
-# plain d9
-docker build -f Dockerfile-d9 \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9 \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim .
-
-# d9 with contrib and nonfree
-docker build -f Dockerfile-d9nf \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree .
-
-# d9 with molecule dependencies
-docker build -f Dockerfile-d9-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-molecule .
 
 # d9 with molecule dependencies and contrib & nonfree repos
-docker build -f Dockerfile-d9nf-molecule \
+docker buildx build --push \
+-f Dockerfile-d9nf-molecule \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
-# plain d10
-docker build -f Dockerfile-d10 \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10 \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim .
-
-# d10 with contrib and nonfree
-docker build -f Dockerfile-d10nf \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree .
-
-# d10 with molecule dependencies
-docker build -f Dockerfile-d10-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-molecule \
--t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-molecule .
-
 # d10 with molecule dependencies and contrib & nonfree repos
-docker build -f Dockerfile-d10nf-molecule \
+docker buildx build --push \
+-f Dockerfile-d10nf-molecule \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
 
 # git tag for git cloning in ci images
-docker build -f Dockerfile-git \
+docker buildx build --push \
+-f Dockerfile-git \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
