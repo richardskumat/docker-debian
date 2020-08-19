6e4b1e19b8b579a224047e52f31018ea3f90480c
Richard Skumat <gitlab@richardskumat.com>
2020-08-19 19:42:17 +0100

    update ci
    make build.sh not push images
    
    create a separate push sh script for pushing images

--- docker-push.sh
+++ docker-push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # d9 with molecule dependencies and contrib & nonfree repos
-docker buildx build \
+docker buildx build --push \
 -f Dockerfile-d9nf-molecule \
 --platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule \
@@ -9,7 +9,7 @@ docker buildx build \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
 # d10 with molecule dependencies and contrib & nonfree repos
-docker buildx build \
+docker buildx build --push \
 -f Dockerfile-d10nf-molecule \
 --platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
@@ -17,7 +17,7 @@ docker buildx build \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
 
 # git tag for git cloning in ci images
-docker buildx build \
+docker buildx build --push \
 -f Dockerfile-git \
 --platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
