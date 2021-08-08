92ab462aada089493d34357f9d8e2fd5c32360ba
Richard Skumat <gitlab@richardskumat.com>
2021-08-08 09:03:27 +0100

    rearrange repo, remove buildx and multiarch builds, as I wasn't using them anyway
    waste of time and resources on free service offerings,
    also causing ci runs to fail sometimes

--- buildx/docker-push.sh
+++ buildx/docker-push.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 # d9 with molecule dependencies and contrib & nonfree repos
-docker build \
+docker buildx build --push \
 -f Dockerfile-d9nf-molecule \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
 # d10 with molecule dependencies and contrib & nonfree repos
-docker build \
+docker buildx build --push \
 -f Dockerfile-d10nf-molecule \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
 
 # git tag for git cloning in ci images
-docker build \
+docker buildx build --push \
 -f Dockerfile-git \
+--platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
--- docker-push.sh
+++ docker-push.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 # d9 with molecule dependencies and contrib & nonfree repos
-docker buildx build --push \
+docker build \
 -f Dockerfile-d9nf-molecule \
---platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
 # d10 with molecule dependencies and contrib & nonfree repos
-docker buildx build --push \
+docker build \
 -f Dockerfile-d10nf-molecule \
---platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
 
 # git tag for git cloning in ci images
-docker buildx build --push \
+docker build \
 -f Dockerfile-git \
---platform linux/amd64,linux/386,linux/arm/v7,linux/arm64 \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
+
+# push one last time
+docker push \
+$DOCKERHUB_USER/$DOCKERHUB_PROJECT --all-tags
