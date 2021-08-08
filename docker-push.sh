ca7f0987a0f3e43446cd2c923bef8198c9009c1d
Richard Skumat <gitlab@richardskumat.com>
2021-08-08 09:29:44 +0100

    https://docs.docker.com/engine/reference/commandline/push/
    docker:stable image was still on 19.03x as of 2021/08/08
    
    which didn't support the -a/-all-tags flag

--- docker-push.sh
+++ docker-push.sh
@@ -7,6 +7,8 @@ docker build \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:stretch-slim-nonfree-molecule .
 
+docker run --rm $DOCKERHUB_USER/$DOCKERHUB_PROJECT:9-nonfree-molecule cat /etc/os-release
+
 # d10 with molecule dependencies and contrib & nonfree repos
 docker build \
 -f Dockerfile-d10nf-molecule \
@@ -14,11 +16,18 @@ docker build \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-nonfree-molecule \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:buster-slim-nonfree-molecule .
 
+docker run --rm $DOCKERHUB_USER/$DOCKERHUB_PROJECT:10-nonfree-molecule cat /etc/os-release
+
 # git tag for git cloning in ci images
 docker build \
 -f Dockerfile-git \
 -t $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git .
 
+docker run --rm $DOCKERHUB_USER/$DOCKERHUB_PROJECT:git git --version
+
 # push one last time
+# https://docs.docker.com/engine/reference/commandline/push/
+# docker:stable image was still on 19.03x as of 2021/08/08
+# which didn't support the -a/-all-tags flag
 docker push \
 $DOCKERHUB_USER/$DOCKERHUB_PROJECT --all-tags
